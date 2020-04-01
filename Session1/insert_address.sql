29530c6818026fe596540cd3e62186f1c94ba163
karora1998 <karora1_be16@thapar.edu>
2020-04-01 12:37:09 +0530

    Insert data in address table

---
+++ Session1/insert_address.sql
@@ -0,0 +1,7 @@
+INSERT INTO address
+VALUES 
+('Anand Roa Circle', 'Bangalore', 'Karnataka'), 
+('Worli', 'Mumbai', 'Maharashtra'),
+('Bhadkal Mor',	'Faridabad', 'Haryana'),
+('Leela bhawan', 'Patiala', 'Punjab'),
+('Marathalli', 'Bangalore', 'Karnataka');
