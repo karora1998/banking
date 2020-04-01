c6580e47ba53ae1b94e61c5ee02ef14dc22ee9da
karora1998 <karora1_be16@thapar.edu>
2020-04-01 12:34:05 +0530

    Insert data in users table

---
+++ Session1/insert_users.sql
@@ -0,0 +1,11 @@
+INSERT INTO users
+VALUES ('Manoj', 'Singh', '1998-10-19', 7207372517, 7000);
+
+INSERT INTO users(first_name, last_name, date_of_birth, phone_number)
+VALUES ('Ram', 'Kumar', '1996-04-03', 7605859712);
+
+INSERT INTO users(first_name, last_name, phone_number, salary)
+VALUES 
+('Dhruv', 'Bajaj', 6953912950, 4000),
+('Manoj', 'Wadhwa',7980532641, 3500),
+('Sumit', 'Sachdeva', 6134062666, 5500);
