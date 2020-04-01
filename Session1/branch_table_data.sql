64cdf7c04f49f49d251097847c00764be64b7e75
karora1998 <karora1_be16@thapar.edu>
2020-04-01 12:55:37 +0530

    Create branch table and add data

---
+++ Session1/branch_table_data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE branch(
+branch_name VARCHAR(100),
+ifsc CHAR(12)
+);
+
+INSERT INTO branch
+VALUES 
+('Domlur', 'BANK00001234'),
+('Koramangala', 'BANK00002567'),
+('Domlur', 'BANK00009854'),
+('Indiranagar', 'BANK00006549'),
+('Marathalli', 'BANK00003427');
