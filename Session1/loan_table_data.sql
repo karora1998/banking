bb3301ebb5c82b93a62dd2a68c5f28b6905b68e9
karora1998 <karora1_be16@thapar.edu>
2020-04-01 13:06:40 +0530

    Create loan table and add data

---
+++ Session1/loan_table_data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE loan(
+   loan_amount DECIMAL(10,2),
+   rate_of_interest INT
+);
+
+INSERT INTO loan VALUES
+(30000000, 12),
+(10000000, 14),
+(5000000, 10),
+(8000000, 15),
+(9000000, 10);
