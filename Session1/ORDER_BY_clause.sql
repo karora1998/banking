37b084d7bb63f6564c843211b9d50575c195d7c6
karora1998 <karora1_be16@thapar.edu>
2020-04-01 13:04:06 +0530

    ORDER BY clause

---
+++ Session1/ORDER_BY_clause.sql
@@ -0,0 +1,14 @@
+SELECT account_number, balance 
+FROM accounts 
+ORDER BY 
+balance;
+
+SELECT account_number, balance 
+FROM accounts 
+ORDER BY 
+balance desc;
+
+SELECT account_number, balance 
+FROM accounts 
+ORDER BY balance DESC 
+LIMIT 3;
